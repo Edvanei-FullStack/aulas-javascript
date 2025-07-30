60abd9b2069bd6a0f635f28cc3d981bc7ba8c81d
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-07-30 16:20:40 -0300

    Auterações
    >Usando JS de forma EXTERNA</h1>

---
+++ js/02-meu-script.js
@@ -0,0 +1,23 @@
+
+/* Exercícios */
+
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+
+let UC = 2
+
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+
+ let competencia = " JS de Forma EXTERNA"
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+
+console.log("Estudando a UC" + UC, "e aplicando" + competencia);
+
+
+
+
+
+
+
+
+ 
