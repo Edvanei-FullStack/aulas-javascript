24867e33831dd428fab53cfb0af63efbd60982b9
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-08-06 16:31:28 -0300

    Exemplo 1: objeto com dados de uma pessoa
    //Exemplo 1: objeto com dados de uma pessoa
    const pessoa = {
        nome: "Chaves",
        idade: 8,
        cidade: "São Paulo",
        estado: "SP"
    
    }
    
    console.log(pessoa);
    
    console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos`);

---
+++ js/04-objetos.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+//Exemplo 1: objeto com dados de uma pessoa 
+const pessoa = {
+    nome: "Chaves",
+    idade: 8,
+    cidade: "São Paulo",
+    estado: "SP" 
+
+}
+
+console.log(pessoa);
+
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos`);
+
+
+
+
+
+
+
