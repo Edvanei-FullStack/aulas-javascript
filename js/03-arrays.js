16994315e726aacb8ff744ab09661bff41f28cbb
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-08-06 14:33:25 -0300

    // Relembrando como criar/usar arrays (vetor) //
    // Relembrando como criar/usar arrays (vetor) //
    
    Array com dados diversos

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -8,3 +8,18 @@ let vocalistas = ["Ney", "Dio", "Lemmy", "David Gilmour"];
 
 console.log(vocalistas);
 
+   // Relembrando como criar/usar arrays (vetor) //
+   //No JS, vc pode colocar qualquer coisa em um array
+
+   let seila = 6;
+
+   const coisas = [10, "Senac", "<h2>Oie</h2>", seila, 15.88];
+   console.log(coisas[1]);
+   console.log(coisas[4]);
+
+
+
+   
+   
+
+
