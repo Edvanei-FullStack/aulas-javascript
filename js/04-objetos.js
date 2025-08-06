5238d64241be628e8a6eb309a087074b5cbcac74
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-08-06 16:46:47 -0300

    Exemplo 2: Objeto com Array
    //Exemplo 2: Objeto com Array
    console.log("\nExemplo\n");
    
    const livro = {
        titulo: "O Senhor dos Anéis",
        autor: "J.R.R. Tolkien",
        volumes: [
            "A Sociedade do Anel",
            "As Duas Torres",
            "O Retorno do Rei"
        ]
    }
    
    /*Para acessar algum dos volumes, primeiro passamos pela propriedade(volumes) usando `Ponto` e, chegando nela usamos os 'colchetes' com indice pois se trata de um array.*/
    
    console.log(`Meu Livro preferido é ${livro.volumes[1]}`);

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -1,6 +1,9 @@
 `use strict`;
 
 //Exemplo 1: objeto com dados de uma pessoa 
+console.log("Exempplo 1\n")
+                      //   \n  utilizado para pular linha.
+
 const pessoa = {
     nome: "Chaves",
     idade: 8,
@@ -12,6 +15,27 @@ const pessoa = {
 console.log(pessoa);
 
 console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos`);
+
+
+//Exemplo 2: Objeto com Array
+console.log("\nExemplo\n");
+
+const livro = {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    volumes: [
+        "A Sociedade do Anel",
+        "As Duas Torres",
+        "O Retorno do Rei"
+    ]
+}
+
+/*Para acessar algum dos volumes, primeiro passamos pela propriedade(volumes) usando `Ponto` e, chegando nela usamos os 'colchetes' com indice pois se trata de um array.*/
+
+console.log(`Meu Livro preferido é ${livro.volumes[1]}`);
+
+
+
 
 
 
