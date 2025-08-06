736e5246b3d36ebae4ac74b2580a7f209291be27
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-08-06 14:20:39 -0300

    Auteração js

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,10 @@
 `use strict`;
 
-console.log();
+//Declarando um array (vetor)
+
+let vocalistas = ["Ney", "Dio", "Lemmy", "David Gilmour"];
+
+// console.log(vocalistas[0]) // vai buscar o "Ney"
+
+console.log(vocalistas);
+
