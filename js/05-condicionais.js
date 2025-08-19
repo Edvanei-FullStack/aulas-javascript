fec5f2cf9b4674a59beefb4fe87368bb58000014
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-08-19 14:56:17 -0300

    Condicional Composta
    if(idade < 18){
        console.log("Você é menor de idade!");
    
    }
    
    else{
        console.log("Você é maior de idade, Parabens!");
    
    }

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -17,5 +17,28 @@ if(numero >= 10){
      
 }
 
+console.log("Exemplo 2");
+
+let aluno = "Ozzy";
+let idade = 19;
+
+/* Lógica: verificar se o aluno é maior ou menor de idade*/
+
+
+//Condicional Composta (if/else)
+if(idade < 18){
+    console.log("Você é menor de idade!");
+    
+}
+
+else{
+    console.log("Você é maior de idade, Parabens!");
+    
+}
+
+
+
+
+
 
 
