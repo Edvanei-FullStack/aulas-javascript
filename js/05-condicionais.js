4577e8da741fd300ba6a6b689e997e3656252378
Edvanei-FullStack <edvaneicruz@gmail.com>
2025-08-19 15:18:23 -0300

    Exercício Condicional
    Primeiro exerciícios de condicional

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -27,14 +27,45 @@ let idade = 19;
 
 //Condicional Composta (if/else)
 if(idade < 18){
-    console.log("Você é menor de idade!");
-    
+    console.log("Você é menor de idade!");    
 }
 
 else{
-    console.log("Você é maior de idade, Parabens!");
-    
+    console.log("Você é maior de idade, Parabens!");    
 }
+
+/*Geral
+
+ Exercício Condicional
+ 
+1. Crie duas variáveis conforme a seguir:
+ 
+- Nota 1 (contendo um valor de 0 a 10)
+- Nota 2 (contendo outro valor de 0 a 10)*/
+
+let nota1 = 3;
+
+let nota2 = 3;
+ 
+/* 2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2.*/
+
+let media = ( nota1 + nota2 ) /2;
+console.log(media);
+
+ 
+3. /*Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+Caso contrário, mostre "reprovado".*/
+
+if(media >= 7){
+    console.log("Aprovado!");    
+  
+}
+
+else{ console.log("Reprovado!");
+
+}
+
 
 
 
